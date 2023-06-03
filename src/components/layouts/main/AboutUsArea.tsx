ff8cf3046b86e9c0bf057d43c531bfaa2242c22c
kouphasi <phasi.ok.phasi@gmail.com>
2023-06-03 23:06:36 +0900

    fix: koheiの自己紹介文の変更

--- src/components/layouts/main/AboutUsArea.tsx
+++ src/components/layouts/main/AboutUsArea.tsx
@@ -25,56 +25,56 @@ const AboutUsArea = () => {
         <div className="flex flex-col items-center h-4/5">
           <ProfileLayout
             name="Kohei Miyoshi"
-            about={`私は三好 航平(みよし こうへい)です。\n来年の4月からフルスタックエンジニアになります。\n現在は、Vue.jsとRuby on Railsを使ったアプリケーション作成をインターンで行っております。\nHTML/CSS、JavaScript、Vue.js、Ruby on Rails、pythonなどを扱うことができます。\nあなたの素敵な想いを形にするお手伝いをさせてください。`}
+            about={`三好 航平(みよし こうへい)と申します。\n来年の4月からフルスタックエンジニアとして働きます。\nWeb開発関係のインターンシップを合計約２年間，２社で行っております。\nHTML/CSS、JavaScript、Vue.js、Ruby on Rails, FastAPI(Python)などによるウェブ開発やPythonによる機械学習による研究などの経験があります。また，趣味としてのデスクトップアプリ制作や，ゲーム開発などを行ってきました。\nより良い価値提供をするためのお手伝いをできることを楽しみにしております。`}
             imgUrl={kohei}
             imgAlt="こうへいの画像です"
           />
-          <div className="pc:mx-20 mt-10">
+          <div className="mt-10 pc:mx-20">
             <AboutSkill
               skillName="JavaScript"
-              className="skillBar w-11/12"
+              className="w-11/12 skillBar"
               imgUrl={javascript}
               imgAlt="言語のアイコン"
             />
             <AboutSkill
               skillName="Vue.js"
-              className="skillBar w-10/12"
+              className="w-10/12 skillBar"
               imgUrl={vue}
               imgAlt="言語のアイコン"
             />
             <AboutSkill
               skillName="React.js"
-              className="skillBar w-8/12"
+              className="w-8/12 skillBar"
               imgUrl={react}
               imgAlt="言語のアイコン"
             />
             <AboutSkill
               skillName="Python"
-              className="skillBar w-10/12"
+              className="w-10/12 skillBar"
               imgUrl={python}
               imgAlt="言語のアイコン"
             />
             <AboutSkill
               skillName="Flask"
-              className="skillBar w-4/5"
+              className="w-4/5 skillBar"
               imgUrl={flask}
               imgAlt="言語のアイコン"
             />
             <AboutSkill
               skillName="Django"
-              className="skillBar w-3/5"
+              className="w-3/5 skillBar"
               imgUrl={django}
               imgAlt="言語のアイコン"
             />
             <AboutSkill
               skillName="Ruby on Rails"
-              className="skillBar w-3/4"
+              className="w-3/4 skillBar"
               imgUrl={rails}
               imgAlt="言語のアイコン"
             />
             <AboutSkill
               skillName="Github"
-              className="skillBar w-full"
+              className="w-full skillBar"
               imgUrl={github}
               imgAlt="言語のアイコン"
             />
@@ -88,46 +88,46 @@ const AboutUsArea = () => {
             imgUrl={takahiro}
             imgAlt="たかひろの画像です"
           />
-          <div className="pc:mx-20 mt-10">
+          <div className="mt-10 pc:mx-20">
             <AboutSkill
               skillName="JavaScript"
-              className="skillBar w-4/5"
+              className="w-4/5 skillBar"
               imgUrl={javascript}
               imgAlt="言語のアイコン"
             />
             <AboutSkill
               skillName="Vue.js"
-              className="skillBar w-3/5"
+              className="w-3/5 skillBar"
               imgUrl={vue}
               imgAlt="言語のアイコン"
             />
             <AboutSkill
               skillName="React.js"
-              className="skillBar w-3/4"
+              className="w-3/4 skillBar"
               imgUrl={react}
               imgAlt="言語のアイコン"
             />
             <AboutSkill
               skillName="TypeScript"
-              className="skillBar w-2/3"
+              className="w-2/3 skillBar"
               imgUrl={typescript}
               imgAlt="言語のアイコン"
             />
             <AboutSkill
               skillName="Next.js"
-              className="skillBar w-3/4"
+              className="w-3/4 skillBar"
               imgUrl={next}
               imgAlt="言語のアイコン"
             />
             <AboutSkill
               skillName="Ruby on Rails"
-              className="skillBar w-3/5"
+              className="w-3/5 skillBar"
               imgUrl={rails}
               imgAlt="言語のアイコン"
             />
             <AboutSkill
               skillName="Github"
-              className="skillBar w-full"
+              className="w-full skillBar"
               imgUrl={github}
               imgAlt="言語のアイコン"
             />
