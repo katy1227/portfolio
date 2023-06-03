703f660d4f6c712bbb81c89127c38c6637030720
寺田卓広 <teradatakahiro@takahiroMacBook-Pro.local>
2023-06-03 17:24:49 +0900

    修正

--- src/components/block/WorkProduct.tsx
+++ src/components/block/WorkProduct.tsx
@@ -11,7 +11,7 @@ const WorkProduct = (props: workProduct) => {
         className="mt-5 hoverLink"
       >
         <img src={imgUrl} alt={imgAlt} height={300} width={400} />
-        <h3 className="font-title pc:text-center w-2/3">{title}</h3>
+        <h3 className="font-title mt-5 underline">{title}</h3>
         <div className="font-tag">{tag}</div>
       </a>
     </div>
