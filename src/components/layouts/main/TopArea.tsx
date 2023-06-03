739072f242fc5bc6088f8d79dbb83effad7220a6
寺田卓広 <teradatakahiro@takahiroMacBook-Pro.local>
2023-06-03 17:35:26 +0900

    修正

--- src/components/layouts/main/TopArea.tsx
+++ src/components/layouts/main/TopArea.tsx
@@ -2,9 +2,11 @@ const TopArea = () => {
   return (
     <div id="top" className="bg-architecture-img h-screen bg-cover">
       <div className="h-full flex flex-col text-center text-blue-50 justify-center items-center">
-        <h2 className="top-name">Kohei</h2>
-        <h4 className="font-bold text-5xl drop-shadow-2xl font-name">&</h4>
-        <h2 className="top-name">Takahiro</h2>
+        <h2 className="top-name stroke-black">Kohei</h2>
+        <h4 className="font-bold text-5xl drop-shadow-2xl font-name stroke-black">
+          &
+        </h4>
+        <h2 className="top-name stroke-black">Takahiro</h2>
       </div>
     </div>
   );
